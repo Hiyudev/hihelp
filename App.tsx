5e4d8692899a38893bcbeb5bde4a503140bee315
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-18 20:24:01 -0300

    feat: add logo + theme to use on application

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { SignIn } from "./src/screens/SignIn";
 import { NativeBaseProvider } from "native-base";
+import { THEME } from "./src/styles/theme";
 
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <SignIn />
     </NativeBaseProvider>
   );

0f76b3a541039730ba67afc2e00f588eea7fecfb
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-20 09:58:55 -0300

    feat: add skeleton signin screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,43 @@
-import { Text, View } from "react-native";
+import { VStack, Heading, Icon, useTheme } from "native-base";
+import { Envelope, Key } from "phosphor-react-native";
+import { useState } from "react";
+
+import Logo from "../assets/logo_primary.svg";
+
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
 
 export function SignIn() {
+  const { colors } = useTheme();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignIn() {}
+
   return (
-    <View>
-      <Text>Sign In Page</Text>
-    </View>
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+
+      <Input
+        placeholder="E-mail"
+        mb={4}
+        InputLeftElement={
+          <Icon ml={4} as={<Envelope color={colors.gray[300]} />} />
+        }
+        onChangeText={setEmail}
+      />
+      <Input
+        secureTextEntry
+        mb={8}
+        placeholder="Password"
+        InputLeftElement={<Icon ml={4} as={<Key color={colors.gray[300]} />} />}
+        onChangeText={setPassword}
+      />
+      <Button title="Entrar" w="full" onPress={handleSignIn} />
+    </VStack>
   );
 }

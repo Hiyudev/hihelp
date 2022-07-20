df72164e7be26edd2df6955d10075d239ddcc214
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-20 08:39:10 -0300

    feat: add custom status bar

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
 import { Loading } from "./src/components/Loading";
 
 import { SignIn } from "./src/screens/SignIn";
@@ -17,6 +17,11 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={"transparent"}
+        translucent
+      />
       {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );

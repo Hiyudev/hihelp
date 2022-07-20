eb8bfb6d5407acff5869b302c0e660333f37bda6
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-20 08:36:21 -0300

    feat: add fonts loader

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
-import { SignIn } from "./src/screens/SignIn";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import { NativeBaseProvider } from "native-base";
+import { Loading } from "./src/components/Loading";
+
+import { SignIn } from "./src/screens/SignIn";
 import { THEME } from "./src/styles/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }

55a98723163bf25a8f90bc18ec8a1f0fcd58e1f4
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-26 09:08:53 -0300

    feat: check if user authenticated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,31 @@
 import { NavigationContainer } from "@react-navigation/native";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
+import { useState, useEffect } from "react";
 
 import { SignIn } from "../screens/SignIn";
 import { AppRoutes } from "./App.routes";
+import { Loading } from "../components/Loading";
 
 export function Routes() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((res) => {
+      setUser(res);
+      setIsLoading(false);
+    });
+
+    return subscriber;
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {user ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   );
 }

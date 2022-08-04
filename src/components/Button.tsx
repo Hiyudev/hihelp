c4b57180193f8592b3528b4bf90bdd3a14e89154
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-08-04 17:24:03 -0300

    feat: add button a new prop for error warning

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,25 +2,38 @@ import {
   Button as NativeBaseButton,
   IButtonProps as NativeBaseIButtonProps,
   Heading,
+  Icon,
+  Box,
 } from "native-base";
+import { WarningCircle } from "phosphor-react-native";
 
 type IButtonProps = NativeBaseIButtonProps & {
   title: string;
+  error?: boolean;
 };
 
-export function Button({ title, ...rest }: IButtonProps) {
+export function Button({ title, error, ...rest }: IButtonProps) {
+  const bgColor = error ? "red.700" : "indigo.700";
+  const pressedBgColor = error ? "red.500" : "indigo.500";
+
   return (
     <NativeBaseButton
-      bg="indigo.700"
+      bg={bgColor}
       h={54}
       fontSize="sm"
       rounded="sm"
-      _pressed={{ backgroundColor: "indigo.500" }}
+      _pressed={{ backgroundColor: pressedBgColor }}
       {...rest}
     >
-      <Heading color="white" fontSize="sm">
-        {title}
-      </Heading>
+      {error ? (
+        <Box>
+          <Icon as={<WarningCircle color="white" size={24} />} />
+        </Box>
+      ) : (
+        <Heading color="white" fontSize="sm">
+          {title}
+        </Heading>
+      )}
     </NativeBaseButton>
   );
 }

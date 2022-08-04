4c8b7dcf269e3e52ffed13fae65f0945a6557dde
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-08-04 18:53:04 -0300

    feat: increase minimum characters for passwords

--- src/lib/zod/signInValidation.ts
+++ src/lib/zod/signInValidation.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const SignInFormValidation = z.object({
   email: z.string().email({ message: "E-mail não é válido" }),
-  password: z.string().min(4, { message: "Por favor insira uma senha contendo mais de 4 caracteres" }),
+  password: z.string().min(6, { message: "Por favor insira uma senha contendo mais de 6 caracteres" }),
 });
--- src/lib/zod/signUpValidation.ts
+++ src/lib/zod/signUpValidation.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const SignUpFormValidation = z.object({
   email: z.string().email({ message: "E-mail não é válido" }),
-  password: z.string().min(4, { message: "Por favor insira uma senha contendo mais de 4 caracteres" }),
-  confirmPassword: z.string().min(4, { message: "Por favor insira uma senha contendo mais de 4 caracteres" }),
+  password: z.string().min(6, { message: "Por favor insira uma senha contendo mais de 6 caracteres" }),
+  confirmPassword: z.string().min(6, { message: "Por favor insira uma senha contendo mais de 6 caracteres" }),
 }).refine((data) => data.password === data.confirmPassword, {
   path: ["confirmPassword"],
   message: "As senhas não conferem. Verifique novamente se as senhas foram digitadas de forma correta."

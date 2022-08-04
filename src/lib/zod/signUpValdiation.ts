9f26dbb14ab8e071142eacafa4f4336422dafb86
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-08-04 17:56:07 -0300

    feat: add pre-validation on signup

---
+++ src/lib/zod/signUpValdiation.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const SignUpFormValidation = z.object({
+  email: z.string().email({ message: "E-mail não é válido" }),
+  password: z.string().min(4, { message: "Por favor insira uma senha contendo mais de 4 caracteres" }),
+  confirmPassword: z.string().min(4, { message: "Por favor insira uma senha contendo mais de 4 caracteres" }),
+}).refine((data) => data.password === data.confirmPassword, {
+  path: ["confirmPassword"],
+  message: "As senhas não conferem. Verifique novamente se as senhas foram digitadas de forma correta."
+});
--- src/screens/auth/SignUp.tsx
+++ src/screens/auth/SignUp.tsx
@@ -8,8 +8,9 @@ import {
   useTheme,
   VStack,
   Text,
+  FormControl,
 } from "native-base";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Alert } from "react-native";
 import auth from "@react-native-firebase/auth";
 import { useNavigation } from "@react-navigation/native";
@@ -19,14 +20,23 @@ import Logo from "../../assets/Logo.svg";
 
 import { Button } from "../../components/Button";
 import { Input, ShowInput } from "../../components/Input";
+import { SignUpFormValidation } from "../../lib/zod/signUpValdiation";
+import { z } from "zod";
 
 export function SignUp() {
   const { colors } = useTheme();
   const { toggleColorMode, colorMode } = useColorMode();
+
   const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [cpassword, setCPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [genericError, setGenerticError] = useState(false);
+
   const navigation = useNavigation();
 
   const bgColor = useColorModeValue("gray.100", "gray.900");
@@ -35,34 +45,52 @@ export function SignUp() {
   const txtColorObj = useColorModeValue(colors.gray[700], colors.gray[300]);
 
   function handleSignUp() {
-    if (!email || !password) {
-      return Alert.alert("Cadastrar", "Informe e-mail e senha.");
-    }
-
-    if (!cpassword || cpassword !== password) {
-      return Alert.alert("Cadastrar", "As senhas não se coencidem.");
-    }
-
-    setIsLoading(true);
-
-    auth()
-      .createUserWithEmailAndPassword(email, password)
-      .catch((error) => {
-        console.log(error);
-        const errorCode = error.code;
-        setIsLoading(false);
-
-        switch (errorCode) {
-          case "auth/invalid-email":
-            return Alert.alert("Cadastrar", "E-mail inválido.");
-          case "auth/weak-password":
-            return Alert.alert("Cadastrar", "Senha fraca.");
-          case "auth/email-already-in-use":
-            return Alert.alert("Cadastrar", "E-mail já em uso.");
-          default:
-            return Alert.alert("Cadastrar", "Erro ao entrar.");
-        }
+    try {
+      SignUpFormValidation.parse({
+        email,
+        password,
+        confirmPassword,
       });
+
+      setIsLoading(true);
+
+      auth()
+        .createUserWithEmailAndPassword(email, password)
+        .catch((error) => {
+          console.log(error);
+          const errorCode = error.code;
+          setIsLoading(false);
+
+          switch (errorCode) {
+            case "auth/invalid-email":
+              return Alert.alert("Cadastrar", "E-mail inválido.");
+            case "auth/weak-password":
+              return Alert.alert("Cadastrar", "Senha fraca.");
+            case "auth/email-already-in-use":
+              return Alert.alert("Cadastrar", "E-mail já em uso.");
+            default:
+              return Alert.alert("Cadastrar", "Erro ao entrar.");
+          }
+        });
+    } catch (err) {
+      if (err instanceof z.ZodError) {
+        const issues = err.issues;
+        if (issues.length > 0) {
+          issues.map((issue) => {
+            const { path, message } = issue;
+            if (path[0] === "email") {
+              setEmailError(message);
+            } else if (path[0] === "password") {
+              setPasswordError(message);
+            } else if (path[0] === "confirmPassword") {
+              setConfirmPasswordError(message);
+            }
+          });
+
+          setGenerticError(true);
+        }
+      }
+    }
   }
 
   const handleGoSignIn = () => {
@@ -72,6 +100,19 @@ export function SignUp() {
   const handleChangeTheme = () => {
     toggleColorMode();
   };
+
+  const hasEmailError = emailError.length > 0;
+  const hasPasswordError = passwordError.length > 0;
+  const hasConfirmPasswordError = confirmPasswordError.length > 0;
+
+  useEffect(() => {
+    if (!genericError) return;
+
+    setEmailError("");
+    setPasswordError("");
+    setConfirmPasswordError("");
+    setGenerticError(false);
+  }, [password, email, confirmPassword]);
 
   return (
     <>
@@ -95,31 +136,55 @@ export function SignUp() {
           <Logo color={useColorModeValue("black", "white")} />
         </HStack>
 
-        <Heading color={txtColor} fontSize="xl" mt={20} mb={6}>
+        <Heading color={txtColor} fontSize="xl" mt={16} mb={6}>
           Cadastre uma nova conta
         </Heading>
 
-        <Input
-          placeholder="E-mail"
-          mb={4}
-          InputLeftElement={
-            <Icon ml={4} as={<Envelope color={txtColorObj} />} />
-          }
-          onChangeText={setEmail}
-        />
-        <ShowInput
-          mb={4}
-          placeholder="Senha"
-          InputLeftElement={<Icon ml={4} as={<Key color={txtColorObj} />} />}
-          onChangeText={setPassword}
-        />
-        <ShowInput
-          mb={8}
-          placeholder="Confirme a senha"
-          InputLeftElement={<Icon ml={4} as={<Key color={txtColorObj} />} />}
-          onChangeText={setCPassword}
-        />
+        <FormControl mb={hasEmailError ? 0 : 6} isInvalid={hasEmailError}>
+          <FormControl.Label>E-mail</FormControl.Label>
+
+          <Input
+            error={hasEmailError}
+            placeholder="Coloque seu e-mail"
+            InputLeftElement={
+              <Icon ml={4} as={<Envelope color={txtColorObj} />} />
+            }
+            onChangeText={setEmail}
+          />
+          <FormControl.ErrorMessage>{emailError}</FormControl.ErrorMessage>
+        </FormControl>
+
+        <FormControl mb={hasPasswordError ? 0 : 6} isInvalid={hasPasswordError}>
+          <FormControl.Label>Senha</FormControl.Label>
+
+          <ShowInput
+            error={hasPasswordError}
+            placeholder="Coloque sua senha"
+            InputLeftElement={<Icon ml={4} as={<Key color={txtColorObj} />} />}
+            onChangeText={setPassword}
+          />
+          <FormControl.ErrorMessage>{passwordError}</FormControl.ErrorMessage>
+        </FormControl>
+
+        <FormControl
+          mb={hasConfirmPasswordError ? 2 : 8}
+          isInvalid={hasConfirmPasswordError}
+        >
+          <FormControl.Label>Confirme a senha</FormControl.Label>
+
+          <ShowInput
+            error={hasConfirmPasswordError}
+            placeholder="Repita a sua senha"
+            InputLeftElement={<Icon ml={4} as={<Key color={txtColorObj} />} />}
+            onChangeText={setConfirmPassword}
+          />
+          <FormControl.ErrorMessage>
+            {confirmPasswordError}
+          </FormControl.ErrorMessage>
+        </FormControl>
+
         <Button
+          error={genericError}
           isLoading={isLoading}
           title="Cadastrar"
           w="full"

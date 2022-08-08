d1c208ae46f9e57369880dce995b9f0a3f5b817d
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-08-08 10:01:17 -0300

    feat: add on details screen pre-validation on form

--- src/screens/app/Details.tsx
+++ src/screens/app/Details.tsx
@@ -1,11 +1,13 @@
 import firestore from "@react-native-firebase/firestore";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import {
+  FormControl,
   HStack,
   ScrollView,
   Text,
   useColorModeValue,
   useTheme,
+  useToast,
   VStack,
 } from "native-base";
 import {
@@ -15,15 +17,17 @@ import {
   ClipboardText,
 } from "phosphor-react-native";
 import { useEffect, useState } from "react";
-import { Alert } from "react-native";
+import { z } from "zod";
+
 import { Button } from "../../components/Button";
 import { CardDetails } from "../../components/CardDetails";
-
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { Loading } from "../../components/Loading";
 import { OrderProps } from "../../components/Order";
+import { Toast } from "../../components/Toast";
 import { OrderFirestoreDTO } from "../../DTOs/OrderFirestoreDTO";
+import { DetailsFormValidation } from "../../lib/zod/detailsValidation";
 import { formatFirestoreDate } from "../../utils/firestoreDateFormat";
 
 type RouteParams = {
@@ -38,13 +42,18 @@ type OrderDetails = OrderProps & {
 
 export function Details() {
   const navigation = useNavigation();
+  const toast = useToast();
   const route = useRoute();
   const { colors } = useTheme();
   const { orderId } = route.params as RouteParams;
 
   const [isLoading, setIsLoading] = useState(true);
   const [solution, setSolution] = useState("");
+  const [solutionError, setSolutionError] = useState("");
   const [order, setOrder] = useState<OrderDetails>({} as OrderDetails);
+  const [genericError, setGenericError] = useState(false);
+
+  const hasSolutionError = solutionError.length > 0;
 
   const bgColor = useColorModeValue("gray.100", "gray.900");
   const secondaryBgColor = useColorModeValue("gray.200", "gray.800");
@@ -53,30 +62,72 @@ export function Details() {
   const greenColor = useColorModeValue(colors.green[700], colors.green[500]);
 
   const handleCloseOrder = () => {
-    if (!solution) {
-      return Alert.alert(
-        "Solicitação",
-        "Por favor, informe a solução do problema para encerrar"
-      );
-    }
-
-    firestore()
-      .collection<OrderFirestoreDTO>("orders")
-      .doc(orderId)
-      .update({
-        status: "closed",
+    try {
+      DetailsFormValidation.parse({
         solution,
-        closedAt: firestore.FieldValue.serverTimestamp(),
-      })
-      .then(() => {
-        Alert.alert("Solicitação", "Solução encerrada com sucesso");
-        navigation.goBack();
-      })
-      .catch((error) => {
-        console.log(error);
-        Alert.alert("Solicitação", "Não foi possível encerrar a solicitação");
       });
+
+      firestore()
+        .collection<OrderFirestoreDTO>("orders")
+        .doc(orderId)
+        .update({
+          status: "closed",
+          solution,
+          closedAt: firestore.FieldValue.serverTimestamp(),
+        })
+        .then(() => {
+          toast.show({
+            render: () => (
+              <Toast
+                style="success"
+                message={"Solução encerrada com sucesso"}
+              />
+            ),
+          });
+          navigation.goBack();
+        })
+        .catch((error) => {
+          console.log(error);
+          toast.show({
+            render: () => (
+              <Toast
+                style="danger"
+                message={"Não foi possível encerrar a solicitação"}
+              />
+            ),
+          });
+        });
+    } catch (err) {
+      if (err instanceof z.ZodError) {
+        const issues = err.issues;
+
+        if (issues.length > 0) {
+          issues.map((issue) => {
+            const { path, message } = issue;
+
+            toast.show({
+              render: () => <Toast style="danger" message={message} />,
+            });
+
+            switch (path[0]) {
+              case "solution":
+                setSolutionError(message);
+                break;
+            }
+          });
+
+          setGenericError(true);
+        }
+      }
+    }
   };
+
+  useEffect(() => {
+    if (!genericError) return;
+
+    setGenericError(false);
+    setSolutionError("");
+  }, [solution]);
 
   useEffect(() => {
     firestore()
@@ -155,19 +206,30 @@ export function Details() {
           footer={order.closed && `Encerrado me ${order.closed}`}
         >
           {order.status == "open" && (
-            <Input
-              placeholder="Descrição da solução"
-              onChangeText={setSolution}
-              h={24}
-              textAlignVertical="top"
-              multiline
-            />
+            <FormControl isInvalid={hasSolutionError}>
+              <Input
+                placeholder="Descrição da solução"
+                onChangeText={setSolution}
+                h={24}
+                error={hasSolutionError}
+                textAlignVertical="top"
+                multiline
+              />
+              <FormControl.ErrorMessage>
+                {solutionError}
+              </FormControl.ErrorMessage>
+            </FormControl>
           )}
         </CardDetails>
       </ScrollView>
 
       {order.status == "open" && (
-        <Button onPress={handleCloseOrder} title="Encerrar solicitação" m={5} />
+        <Button
+          error={genericError}
+          onPress={handleCloseOrder}
+          title="Encerrar solicitação"
+          m={5}
+        />
       )}
     </VStack>
   );

4da86e6a9ae0b1dc38fd50ea89b164fcb04ffe80
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-08-08 10:31:47 -0300

    refactor: fix typo

--- src/lib/zod/detailsValidation.ts
+++ src/lib/zod/detailsValidation.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
 
 export const DetailsFormValidation = z.object({
-  solution: z.string().min(2, { message: "A solução deve conter ao menos 2 carateres para ser encerrado." }),
+  solution: z.string().min(2, { message: "A solução deve conter ao menos 2 carateres para ser encerrado" }),
 });
